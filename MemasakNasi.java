0d70b268ce20ba9f031473380b75891d310a7c9f
rusmana123 <32738214+rusmana123@users.noreply.github.com>
2017-10-18 20:05:37 +0700

    Add files via upload

---
+++ MemasakNasi.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package memasak.nasi;
+
+/**
+ *
+ * @author Asus PC
+ */
+public class MemasakNasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("cara memasak nasi pertama masukan beras kedalam wadah magiccom"
+                + "lalu bersihkan beras dengan air sesudah di bersihkan ganti air nya dengan air bersih "
+                + "setelah itu masukan wadah magiccom ke magiccom lalu nyalakan listrik tunggu beberapa menit hingga matang "
+                );  }
+    
+}
